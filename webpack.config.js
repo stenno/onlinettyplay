5b095dad67c4a23962e4a5ab52338662d887372e
stenno <stenno@stenno.dev>
2021-12-14 02:59:48 +0100

    clean /dist in prod

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ const config = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        clean: isProduction,
     },
     plugins: [
         new HtmlWebpackPlugin({
